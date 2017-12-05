4674facbb6a71834373e59e553d14aa0334ad0f6
Paul-Hughes <Paul.Hughes@marklogic.com>
2017-12-05 12:21:42 -0800

    Initial commit.

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var EmailBuilder = require('gulp-email-builder');
+
+var options = { encodeSpecialChars: true }
+var builder = EmailBuilder(options);
+
+gulp.task('sass', function () {
+    gulp.src('./scss/inline/inline.scss')
+        .pipe(sass({
+            includePaths: [
+                './scss/inline'
+            ],
+            outputStyle: 'nested'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+    gulp.src('./scss/embedded/embedded.scss')
+        .pipe(sass({
+            includePaths: [
+                './scss/embedded'
+            ],
+            outputStyle: 'nested'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('emailBuilder', function() {
+    return gulp.src('./html/*.html')
+        .pipe(builder.build())
+        .pipe(gulp.dest('./dist/'));
+});
+
+//Watch task
+gulp.task('default', ['sass', 'emailBuilder', 'watch']);
+
+gulp.task('watch',function() {
+    gulp.watch('scss/**/*.scss',['sass', 'emailBuilder']);
+    gulp.watch('html/*.html',['emailBuilder']);
+});
